ea214bbeb589b1a3a553b6db665b6f035a7c277a
aleksandrtretyakov <tretyakov.aa74+2@gmail.com>
2022-04-16 15:48:55 +0300

    added second test

--- src/test/java/guru/qa/HomeWorkTest.java
+++ src/test/java/guru/qa/HomeWorkTest.java
@@ -3,6 +3,7 @@ package guru.qa;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.visible;
@@ -25,6 +26,21 @@ public class HomeWorkTest {
         $(byText(testData)).click();
 //        Expected result
         $(".css-truncate-target").parent().shouldBe(visible);
+
+    }
+
+    @CsvSource({
+            "Try Kotlin, Simple"
+    })
+
+    @ParameterizedTest(name = "For {0} app we should have {1} tab")
+    void checkingOtherSite(String name, String text) {
+//        Pre-condition
+        Selenide.open("https://www.jetbrains.com/ru-ru/");
+//        Steps
+        $(byText("Знакомство с Kotlin")).click();
+//        Expected result
+        $(".kotlin-overview-code-example-tabs__group").shouldHave(Condition.attribute(text));
 
     }
 }
